232155fd2a3642444d042cc59e3b6bd5f5e8c4e3
yurimarca <yurimarca@gmail.com>
2023-10-28 04:23:25 -0300

    latex-cv now renders a pdf file from latex file and download it

--- latex-cv/app.py
+++ latex-cv/app.py
@@ -1,10 +1,34 @@
-from flask import Flask
+from flask import Flask, send_file
+import subprocess
+import os
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def index():
+    return 'Hello, visit /render to render the LaTeX file.'
+
+@app.route('/render')
+def render_latex():
+    latex_code = r'''
+    \documentclass{article}
+    \begin{document}
+    Hello, this is a simple LaTeX document.
+    \end{document}
+    '''
+    with open('temp.tex', 'w') as f:
+        f.write(latex_code)
+
+    try:
+        subprocess.run(['pdflatex', 'temp.tex'], check=True)
+    except subprocess.CalledProcessError:
+        return 'Failed to compile LaTeX'
+
+    for ext in ['.aux', '.log']:
+        os.remove(f'temp{ext}')
+
+    return send_file('temp.pdf', as_attachment=True, download_name='rendered.pdf')
+
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=80)
+    app.run(host='0.0.0.0', port=5000)
